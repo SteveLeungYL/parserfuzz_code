f232aeb12d6db50bcd78af9f8bb599dd36553dbf
Yu Liang <luy70@psu.edu>
2021-04-17 15:37:05 -0400

    Introduce three different mutation methods for the get_random_mutated_norec_select_stmt func

--- SQLite/src/mutator.cpp
+++ SQLite/src/mutator.cpp
@@ -185,15 +185,25 @@ string Mutator::get_random_mutated_norec_select_stmt(){
   while (!is_success) {
 
     string ori_norec_select = "";
-    /* One third of the time, we will grab one query from the query library, if the query library contians anything. */
+    /* One third of the time, we will grab one query from the query library, if the query library contians anything. 
+        Another one third of the time, we will grab one query from the query library, mutate them, and return the mutated version of the query.
+        At last, one third of the time, we grab query from the templates, "SELECT COUNT ( * ) FROM v0 WHERE v1 ; ", mutate it, and return. 
+    */
     int query_method = get_rand_int(3);
-    if (all_norec_pstr_vec.size() > 0 && query_method < 1)  {
+    if (all_norec_pstr_vec.size() > 0 && query_method == 0)  {
       /* Pick the query from the lib, directly return, do not mutate it. (If mutate, could have significantly performance penalty.) */
       ori_norec_select = *(all_norec_pstr_vec[get_rand_int(all_norec_pstr_vec.size())]);
       if (ori_norec_select == "" || !is_norec_compatible(ori_norec_select)) continue;
       use_temp = false;
-      // return ori_norec_select;
+      return ori_norec_select;  
+    }
+    else if (all_norec_pstr_vec.size() > 0 && query_method == 1) {
+      /* Pick the query from the lib, pass to the mutator. */
+      ori_norec_select = *(all_norec_pstr_vec[get_rand_int(all_norec_pstr_vec.size())]);
+      if (ori_norec_select == "" || !is_norec_compatible(ori_norec_select)) continue;
+      use_temp = false;
     } else {
+      /* Pick the query from the template, pass to the mutator. */
       ori_norec_select = "SELECT COUNT ( * ) FROM v0 WHERE v1 ; ";
       use_temp = true;
     }
