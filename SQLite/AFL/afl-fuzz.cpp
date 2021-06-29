b2036f005933e166e876b158175f68a226466801
Hong Hu <huhong789@gmail.com>
2021-06-29 14:51:30 -0400

    disable fuzzer_status_currectness

--- SQLite/AFL/afl-fuzz.cpp
+++ SQLite/AFL/afl-fuzz.cpp
@@ -5519,15 +5519,15 @@ EXP_ST u8 common_fuzz_stuff(char **argv, string &query_str) {
   if (!(stage_cur % stats_update_freq) || stage_cur + 1 == stage_max)
     show_stats();
 
-  ofstream outputfile;
-  char * bug_output_dir_char = 
-      (char *)alloc_printf("%s/fuzzer_stats_correctness", out_dir);
-  string bug_output_dir = (const char *) bug_output_dir_char;
-  outputfile.open(bug_output_dir, std::ofstream::out | std::ofstream::trunc);
-  print_exec_debug_info(outputfile);
-  outputfile.close();
+  //ofstream outputfile;
+  //char * bug_output_dir_char = 
+  //    (char *)alloc_printf("%s/fuzzer_stats_correctness", out_dir);
+  //string bug_output_dir = (const char *) bug_output_dir_char;
+  //outputfile.open(bug_output_dir, std::ofstream::out | std::ofstream::trunc);
+  //print_exec_debug_info(outputfile);
+  //outputfile.close();
 
-  ck_free(bug_output_dir_char);
+  //ck_free(bug_output_dir_char);
 
   return 0;
 }
