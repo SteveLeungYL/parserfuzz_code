e92e685a95e1379baa3c2de64c9404756deab0f4
Yu Liang <luy70@psu.edu>
2021-09-07 00:00:38 -0400

    bad idea to reduce EXEC_TIMEOUT

--- SQLite/AFL/config.h
+++ SQLite/AFL/config.h
@@ -48,7 +48,7 @@
 /* Default timeout for fuzzed code (milliseconds). This is the upper bound,
    also used for detecting hangs; the actual value is auto-scaled: */
 
-#define EXEC_TIMEOUT 300
+#define EXEC_TIMEOUT 1000
 
 /* Timeout rounding factor when auto-scaling (milliseconds): */
 
