4f68fdeb78cb8af1d3a075758c9b4046e189f9f6
Hong Hu <huhong789@gmail.com>
2021-10-24 10:17:13 -0400

    keyword lookup

--- PostgreSQL/parser/parser.cpp
+++ PostgreSQL/parser/parser.cpp
@@ -22,6 +22,8 @@ static bool check_uescapechar(unsigned char escape);
 void truncate_identifier(char *ident, int len, bool warn);
 char * downcase_truncate_identifier(const char *ident, int len, bool warn);
 char * downcase_identifier(const char *ident, int len, bool warn, bool truncate);
+int strtoint(const char *pg_restrict str, char **pg_restrict endptr, int base);
+int ScanKeywordLookup(const char *str, const ScanKeywordList *keywords);
 
 /*
  * raw_parser
@@ -599,4 +601,85 @@ downcase_identifier(const char *ident, int len, bool warn, bool truncate)
 		truncate_identifier(result, i, warn);
 
 	return result;
+}
+
+/*
+ * strtoint --- just like strtol, but returns int not long
+ */
+int
+strtoint(const char *pg_restrict str, char **pg_restrict endptr, int base)
+{
+	long		val;
+
+	val = strtol(str, endptr, base);
+	if (val != (int) val)
+		errno = ERANGE;
+	return (int) val;
+}
+
+
+/*
+ * ScanKeywordLookup - see if a given word is a keyword
+ *
+ * The list of keywords to be matched against is passed as a ScanKeywordList.
+ *
+ * Returns the keyword number (0..N-1) of the keyword, or -1 if no match.
+ * Callers typically use the keyword number to index into information
+ * arrays, but that is no concern of this code.
+ *
+ * The match is done case-insensitively.  Note that we deliberately use a
+ * dumbed-down case conversion that will only translate 'A'-'Z' into 'a'-'z',
+ * even if we are in a locale where tolower() would produce more or different
+ * translations.  This is to conform to the SQL99 spec, which says that
+ * keywords are to be matched in this way even though non-keyword identifiers
+ * receive a different case-normalization mapping.
+ */
+int
+ScanKeywordLookup(const char *str,
+				  const ScanKeywordList *keywords)
+{
+	size_t		len;
+	int			h;
+	const char *kw;
+
+	/*
+	 * Reject immediately if too long to be any keyword.  This saves useless
+	 * hashing and downcasing work on long strings.
+	 */
+	len = strlen(str);
+	if (len > keywords->max_kw_len)
+		return -1;
+
+	/*
+	 * Compute the hash function.  We assume it was generated to produce
+	 * case-insensitive results.  Since it's a perfect hash, we need only
+	 * match to the specific keyword it identifies.
+	 */
+	h = keywords->hash(str, len);
+
+	/* An out-of-range result implies no match */
+	if (h < 0 || h >= keywords->num_keywords)
+		return -1;
+
+	/*
+	 * Compare character-by-character to see if we have a match, applying an
+	 * ASCII-only downcasing to the input characters.  We must not use
+	 * tolower() since it may produce the wrong translation in some locales
+	 * (eg, Turkish).
+	 */
+	kw = GetScanKeyword(h, keywords);
+	while (*str != '\0')
+	{
+		char		ch = *str++;
+
+		if (ch >= 'A' && ch <= 'Z')
+			ch += 'a' - 'A';
+		if (ch != *kw++)
+			return -1;
+	}
+	if (*kw != '\0')
+		return -1;
+
+	/* Success! */
+	return h;
 }
