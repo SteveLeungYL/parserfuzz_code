27c0d1745aecb31e4b4283411eb14c3501f90a77
Yu Liang <luy70@psu.edu>
2021-11-21 15:13:57 -0500

    Reorganize inputs and postgres_initlib folders. Includes inputs from pgTAP.

---
+++ PostgreSQL/fuzz_root/inputs/comments.sql
@@ -0,0 +1,3 @@
+SELECT 'trailing' AS first; SELECT 'before multi-line' AS fourth;
+SELECT 'after multi-line' AS fifth;
+SELECT Hoo boy. Still two deep...Now just one deep...'deeply nested example' AS sixth;
