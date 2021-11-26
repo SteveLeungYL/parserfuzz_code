67ebbe9f9e94d6bed168a1bbc5ddf698295c3eaf
Yu Liang <luy70@psu.edu>
2021-11-26 18:42:39 -0500

    Parser Demo. WIP.

--- MySQL/parser/sql_parse_entry.cpp
+++ MySQL/parser/sql_parse_entry.cpp
@@ -1,168 +1,99 @@
 #include "./sql_parse_entry.h"
-#include "./sql/sql_parse.h"
-#include "../include/ast.h"
-
-#include <algorithm>
-#include <atomic>
-#include <climits>
-#include <cstdint>
-#include <cstdio>
-#include <cstdlib>
-#include <cstring>
-#include <functional>
-#include <iterator>
-#include <memory>
-#include <optional>
-#include <string>
-#include <utility>
-#include <vector>
-
-#include "my_config.h"
-#ifdef HAVE_LSAN_DO_RECOVERABLE_LEAK_CHECK
-#include <sanitizer/lsan_interface.h>
-#endif
-
-#include "dur_prop.h"
-#include "field_types.h"  // enum_field_types
-#include "m_ctype.h"
-#include "m_string.h"
-#include "mem_root_deque.h"
-#include "my_alloc.h"
-#include "my_compiler.h"
-#include "my_dbug.h"
-#include "my_hostname.h"
-#include "my_inttypes.h"  // TODO: replace with cstdint
-#include "my_io.h"
-#include "my_loglevel.h"
-#include "my_macros.h"
-#include "my_psi_config.h"
-#include "my_sys.h"
-#include "my_table_map.h"
-#include "my_thread_local.h"
-#include "my_time.h"
-#include "mysql/com_data.h"
-#include "mysql/components/services/bits/plugin_audit_connection_types.h"  // MYSQL_AUDIT_CONNECTION_CHANGE_USER
-#include "mysql/components/services/log_builtins.h"        // LogErr
-#include "mysql/components/services/psi_statement_bits.h"  // PSI_statement_info
-#include "mysql/plugin_audit.h"
-#include "mysql/psi/mysql_mutex.h"
-#include "mysql/psi/mysql_rwlock.h"
-#include "mysql/psi/mysql_statement.h"
-#include "mysql/service_mysql_alloc.h"
-#include "mysql/udf_registration_types.h"
-#include "mysql_version.h"
-#include "mysqld_error.h"
-#include "mysys_err.h"  // EE_CAPACITY_EXCEEDED
-#include "pfs_thread_provider.h"
-#include "prealloced_array.h"
-#include "scope_guard.h"
-#include "sql/auth/auth_acls.h"
-#include "sql/auth/auth_common.h"  // acl_authenticate
-#include "sql/auth/sql_security_ctx.h"
-#include "sql/binlog.h"  // purge_master_logs
-#include "sql/clone_handler.h"
-#include "sql/comp_creator.h"
-#include "sql/create_field.h"
-#include "sql/current_thd.h"
-#include "sql/dd/cache/dictionary_client.h"  // dd::cache::Dictionary_client::Auto_releaser
-#include "sql/dd/dd.h"                       // dd::get_dictionary
-#include "sql/dd/dd_schema.h"                // Schema_MDL_locker
-#include "sql/dd/dictionary.h"  // dd::Dictionary::is_system_view_name
-#include "sql/dd/info_schema/table_stats.h"
-#include "sql/dd/types/column.h"
-#include "sql/debug_sync.h"  // DEBUG_SYNC
-#include "sql/derror.h"      // ER_THD
-#include "sql/discrete_interval.h"
-#include "sql/error_handler.h"  // Strict_error_handler
-#include "sql/events.h"         // Events
-#include "sql/field.h"
-#include "sql/gis/srid.h"
-#include "sql/item.h"
-#include "sql/item_cmpfunc.h"
-#include "sql/item_func.h"
-#include "sql/item_subselect.h"
-#include "sql/item_timefunc.h"  // Item_func_unix_timestamp
-#include "sql/key_spec.h"       // Key_spec
-#include "sql/locked_tables_list.h"
-#include "sql/log.h"        // query_logger
-#include "sql/log_event.h"  // slave_execute_deferred_events
-#include "sql/mdl.h"
-#include "sql/mem_root_array.h"
-#include "sql/mysqld.h"              // stage_execution_of_init_command
-#include "sql/mysqld_thd_manager.h"  // Find_thd_with_id
-#include "sql/nested_join.h"
-#include "sql/opt_hints.h"
-#include "sql/opt_trace.h"  // Opt_trace_start
-#include "sql/parse_location.h"
-#include "sql/parse_tree_node_base.h"
-#include "sql/parse_tree_nodes.h"
-#include "sql/parser_yystype.h"
-#include "sql/persisted_variable.h"
-#include "sql/protocol.h"
-#include "sql/protocol_classic.h"
-#include "sql/psi_memory_key.h"
-#include "sql/query_options.h"
-#include "sql/query_result.h"
-#include "sql/resourcegroups/resource_group_basic_types.h"
-#include "sql/resourcegroups/resource_group_mgr.h"  // Resource_group_mgr::instance
-#include "sql/rpl_context.h"
-#include "sql/rpl_filter.h"             // rpl_filter
-#include "sql/rpl_group_replication.h"  // group_replication_start
-#include "sql/rpl_gtid.h"
-#include "sql/rpl_handler.h"  // launch_hook_trans_begin
-#include "sql/rpl_replica.h"  // change_master_cmd
-#include "sql/rpl_source.h"   // register_slave
-#include "sql/rpl_utility.h"
-#include "sql/session_tracker.h"
-#include "sql/set_var.h"
-#include "sql/sp.h"        // sp_create_routine
-#include "sql/sp_cache.h"  // sp_cache_enforce_limit
-#include "sql/sp_head.h"   // sp_head
-#include "sql/sql_admin.h"
-#include "sql/sql_alter.h"
-#include "sql/sql_audit.h"   // MYSQL_AUDIT_NOTIFY_CONNECTION_CHANGE_USER
-#include "sql/sql_base.h"    // find_temporary_table
-#include "sql/sql_binlog.h"  // mysql_client_binlog_statement
-#include "sql/sql_check_constraint.h"
-#include "sql/sql_class.h"
-#include "sql/sql_cmd.h"
-#include "sql/sql_connect.h"  // decrease_user_connections
-#include "sql/sql_const.h"
-#include "sql/sql_db.h"  // mysql_change_db
-#include "sql/sql_digest.h"
-#include "sql/sql_digest_stream.h"
-#include "sql/sql_error.h"
-#include "sql/sql_handler.h"  // mysql_ha_rm_tables
-#include "sql/sql_help.h"     // mysqld_help
-#include "sql/sql_lex.h"
-#include "sql/sql_list.h"
-#include "sql/sql_prepare.h"  // mysql_stmt_execute
-#include "sql/sql_profile.h"
-#include "sql/sql_query_rewrite.h"  // invoke_pre_parse_rewrite_plugins
-#include "sql/sql_reload.h"         // handle_reload_request
-#include "sql/sql_rename.h"         // mysql_rename_tables
-#include "sql/sql_rewrite.h"        // mysql_rewrite_query
-#include "sql/sql_show.h"           // find_schema_table
-#include "sql/sql_table.h"          // mysql_create_table
-#include "sql/sql_trigger.h"        // add_table_for_trigger
-#include "sql/sql_udf.h"
-#include "sql/sql_view.h"  // mysql_create_view
-#include "sql/strfunc.h"
-#include "sql/system_variables.h"  // System_status_var
-#include "sql/table.h"
-#include "sql/table_cache.h"  // table_cache_manager
-#include "sql/thd_raii.h"
-#include "sql/transaction.h"  // trans_rollback_implicit
-#include "sql/transaction_info.h"
-#include "sql_string.h"
-#include "template_utils.h"
-#include "thr_lock.h"
-#include "violite.h"
-
-extern int MYSQLparse(class THD * thd, class Parse_tree_root * *root);
 
 
-bool parse_sql(THD *thd, Parser_state *parser_state,
+/* Helper functions */
+
+
+const char *ER_THD(const THD *thd, int mysql_errno) {
+  return thd->variables.lc_messages->errmsgs->lookup(mysql_errno);
+}
+
+class Parser_oom_handler : public Internal_error_handler {
+ public:
+  Parser_oom_handler() : m_has_errors(false), m_is_mem_error(false) {}
+  bool handle_condition(THD *thd, uint sql_errno, const char *,
+                        Sql_condition::enum_severity_level *level,
+                        const char *) override {
+    if (*level == Sql_condition::SL_ERROR) {
+      m_has_errors = true;
+      /* Out of memory error is reported only once. Return as handled */
+      if (m_is_mem_error &&
+          (sql_errno == EE_CAPACITY_EXCEEDED || sql_errno == EE_OUTOFMEMORY))
+        return true;
+      if (sql_errno == EE_CAPACITY_EXCEEDED || sql_errno == EE_OUTOFMEMORY) {
+        m_is_mem_error = true;
+        if (sql_errno == EE_CAPACITY_EXCEEDED)
+          my_error(ER_CAPACITY_EXCEEDED, MYF(0),
+                   static_cast<ulonglong>(thd->variables.parser_max_mem_size),
+                   "parser_max_mem_size",
+                   ER_THD(thd, ER_CAPACITY_EXCEEDED_IN_PARSER));
+        else
+          my_error(ER_OUT_OF_RESOURCES, MYF(ME_FATALERROR));
+        return true;
+      }
+    }
+    return false;
+  }
+
+ private:
+  bool m_has_errors;
+  bool m_is_mem_error;
+};
+
+void sp_parser_data::finish_parsing_sp_body(THD *thd) {
+  /*
+    In some cases the parser detects a syntax error and calls
+    THD::cleanup_after_parse_error() method only after finishing parsing
+    the whole routine. In such a situation sp_head::restore_thd_mem_root()
+    will be called twice - the first time as part of normal parsing process
+    and the second time by cleanup_after_parse_error().
+
+    To avoid ruining active arena/mem_root state in this case we skip
+    restoration of old arena/mem_root if this method has been already called
+    for this routine.
+  */
+  if (!is_parsing_sp_body()) return;
+
+  thd->free_items();
+  thd->mem_root = m_saved_memroot;
+  thd->set_item_list(m_saved_item_list);
+
+  m_saved_memroot = nullptr;
+  m_saved_item_list = nullptr;
+}
+
+/**
+  Restore session state in case of parse error.
+
+  This is a clean up function that is invoked after the Bison generated
+  parser before returning an error from THD::sql_parser(). If your
+  semantic actions manipulate with the session state (which
+  is a very bad practice and should not normally be employed) and
+  need a clean-up in case of error, and you can not use %destructor
+  rule in the grammar file itself, this function should be used
+  to implement the clean up.
+*/
+
+void cleanup_after_parse_error(THD* thd) {
+  sp_head *sp = thd->lex->sphead;
+
+  if (sp) {
+    sp->m_parser_data.finish_parsing_sp_body(thd);
+    //  Do not delete sp_head if is invoked in the context of sp execution.
+    if (thd->sp_runtime_ctx == nullptr) {
+      sp_head::destroy(sp);
+      thd->lex->sphead = nullptr;
+    }
+  }
+}
+
+
+
+
+
+/* Main parser entry */ 
+
+bool parse_sql_entry(THD *thd, Parser_state *parser_state,
                Object_creation_ctx *creation_ctx, vector<IR*>& ir_vec) {
   DBUG_TRACE;
   bool ret_value;
@@ -237,12 +168,14 @@ bool parse_sql(THD *thd, Parser_state *parser_state,
   thd->push_diagnostics_area(parser_da, false);
 
   bool mysql_parse_status = false;
-  if (MYSQLparse(this, &root, ir_vec))
+
+  Parse_tree_root *root = nullptr;
+  if (MYSQLparse(thd, &root, ir_vec))
   {
-      cleanup_after_parse_error();
+      cleanup_after_parse_error(thd);
       mysql_parse_status = true;
   }
-  if (root != nullptr && lex->make_sql_cmd(root)) {
+  if (root != nullptr && thd->lex->make_sql_cmd(root)) {
     mysql_parse_status = true;
   }
   mysql_parse_status = false;
@@ -340,7 +273,7 @@ bool parse_sql(THD *thd, Parser_state *parser_state,
 
 
 bool exec_query_command_entry(string input) {
-    THD *thd = init_new_thd(channel_info);
+    THD *thd = new (std::nothrow) THD;
     thd->get_stmt_da()->reset_diagnostics_area();
     thd->get_stmt_da()->reset_statement_cond_count();
 
@@ -372,8 +305,8 @@ bool exec_query_command_entry(string input) {
         // thd->m_parser_state = parser_state;
 
         bool err;
-        err = parse_sql(thd, parser_state, nullptr);
-        thd.end_statement();
+        err = parse_sql(thd, &parser_state, nullptr);
+        thd->end_statement();
     }
 
     thd->release_resources();
@@ -382,4 +315,5 @@ bool exec_query_command_entry(string input) {
     thd->end_statement();
     thd->cleanup_after_query();
 
+    return true;
 }
---
+++ MySQL/parser/sql_parse_entry.h
@@ -0,0 +1,38 @@
+#include "../include/ast.h"
+
+#include <algorithm>
+#include <atomic>
+#include <climits>
+#include <cstdint>
+#include <cstdio>
+#include <cstdlib>
+#include <cstring>
+#include <functional>
+#include <iterator>
+#include <memory>
+#include <optional>
+#include <string>
+#include <utility>
+#include <vector>
+
+#include "my_config.h"
+#ifdef HAVE_LSAN_DO_RECOVERABLE_LEAK_CHECK
+#include <sanitizer/lsan_interface.h>
+#endif
+
+#include "sql/sql_class.h"
+#include "sql/sql_lex.h"
+#include "sql/parse_tree_node_base.h"
+#include "sql/parse_tree_nodes.h"
+#include "sql/table.h"
+#include "sql/sql_parse.h"
+#include "sql/error_handler.h"
+#include "include/mysys_err.h"
+#include "sql/sql_locale.h"
+#include "sql/derror.h"
+#include "sql/sp_head.h"
+
+extern int MYSQLparse(class THD * thd, class Parse_tree_root * *root, vector<IR*> ir_vec);
+
+bool parse_sql_entry(THD *thd, Parser_state *parser_state,
+               Object_creation_ctx *creation_ctx, vector<IR*>& ir_vec);
