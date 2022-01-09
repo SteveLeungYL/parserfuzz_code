5db0976a8faf5cc35895ca09634e7809e0372f40
Yu Liang <luy70@psu.edu>
2022-01-09 15:30:17 -0500

    Small fixes to sqlite scripts.

--- SQLite/fuzz_root/run_parallel.py
+++ SQLite/fuzz_root/run_parallel.py
@@ -8,12 +8,12 @@ import atexit
 oracle_str = "TLP"
 
 
-sqlite_root_dir = "/home/sly/Desktop/SQLRight/mysql_source/mysql-server-mysql-8.0.27/bld/384f5c26f48b92e8bfcb168381d4a8caf3ea59e7_NOR_EARLY"
+sqlite_root_dir = "/home/luy70/Squirrel_DBMS_Project/sqlite3_source/sqlite/bld/384f5c26f48b92e8bfcb168381d4a8caf3ea59e7_NOR_EARLY"
 sqlite_bin = os.path.join(sqlite_root_dir, "sqlite3")
 current_workdir = os.getcwd()
 
 starting_core_id = 0
-parallel_num = 0
+parallel_num = 5
 
 for cur_inst_id in range(starting_core_id, starting_core_id + parallel_num, 1):
     print("###############\nSetting up core_id: " + str(cur_inst_id))
@@ -30,12 +30,12 @@ for cur_inst_id in range(starting_core_id, starting_core_id + parallel_num, 1):
     modi_env["AFL_I_DONT_CARE_ABOUT_MISSING_CRASHES"] = "1"
 
     fuzzing_command = [
-        sqlite_root_dir,
+        "./afl-fuzz",
         "-i", "./inputs",
         "-o", cur_output_dir_str,
         "-c", str(cur_inst_id),
         "-O", str(oracle_str),
-        "--", sqlite_bin,
+        " -- ", sqlite_bin,
         "&"
     ]
 
@@ -54,3 +54,7 @@ for cur_inst_id in range(starting_core_id, starting_core_id + parallel_num, 1):
 
 print("Finished launching the fuzzing. Now monitor the mysql process. ")
 
+print("#############\nFinished launching the fuzzing. \n\n\n")
+while True:
+        time.sleep(100)
+        pass
