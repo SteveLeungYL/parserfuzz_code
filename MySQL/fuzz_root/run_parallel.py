ba6a1a5e8aa9f69a4ec62df374f012a4c592ab14
Yu Liang <luy70@psu.edu>
2022-01-16 19:51:19 -0500

    python script still being killed. Use watch to call python script periodically then.

--- MySQL/fuzz_root/run_parallel.py
+++ MySQL/fuzz_root/run_parallel.py
@@ -9,13 +9,7 @@ import signal
 import psutil
 import MySQLdb
 
-mysql_root_dir = "/home/sly/Desktop/SQLRight/mysql_source/mysql-server-block-inst/mysql-server-mysql-8.0.27/bld_black_list"
-mysql_src_data_dir = os.path.join(mysql_root_dir, "data_all/ori_data")
-current_workdir = os.getcwd()
-
-starting_core_id = 0
-parallel_num = 3
-port_starting_num = 9000
+from afl_config import *
 
 all_fuzzing_p_list = dict()
 all_mysql_p_list = dict()
@@ -153,186 +147,21 @@ for cur_inst_id in range(starting_core_id, starting_core_id + parallel_num, 1):
     time.sleep(1)
     all_mysql_p_list[cur_inst_id] = cur_shm_str
 
+all_share_mem_file = "./all_share_mem_file.txt"
+all_share_mem_file = open(all_share_mem_file, "w")
+
+for cur_inst_id, cur_shm_str in all_mysql_p_list.items():
+    all_share_mem_file.write("%s:%s\n" % (cur_inst_id, cur_shm_str))
+
+all_share_mem_file.flush()
+all_share_mem_file.close()
+
+all_prev_shut_time_file = open("./all_prev_shut_time_file.txt", "w")
+for i in range(starting_core_id, starting_core_id + parallel_num, 1):
+    all_prev_shut_time_file.write("%d:%s\n" % (i, time.mktime(time.localtime())))
+all_prev_shut_time_file.close()
+
 print("Finished launching the fuzzing. Now monitor the mysql process. ")
 
-# An prarallel_num length list. 
-all_prev_shutdown_time = [time.localtime()] * parallel_num
-
-is_Test = True
-
 while True:
-
-    time.sleep(2)
-
-    for cur_inst_id in range(starting_core_id, starting_core_id + parallel_num, 1):
-
-        cur_shm_str = all_mysql_p_list[cur_inst_id]
-
-        cur_port_num = port_starting_num + cur_inst_id - starting_core_id
-        socket_path = "/tmp/mysql_" + str(cur_inst_id) + ".sock"
-
-        is_server_down = False
-        try:
-            db = MySQLdb.connect(host="localhost",    # your host, usually localhost
-                     user="root",         # your username
-                     passwd="",  # your password
-                     port=cur_port_num,
-                     unix_socket=socket_path,
-                     db="fuck")        # name of the data base
-            db.close()
-        except MySQLdb._exceptions.OperationalError:
-            is_server_down = True
-        
-        if not is_server_down:
-            continue
-
-        # for proc in psutil.process_iter():
-        #     # check whether the process name matches
-        #     cur_proc_name = proc.name()
-        #     print(cur_proc_name)
-
-        ### CANNOT FIND THE MYSQL SERVER. CRASHED? 
-        print("*****************\nMySQL Server with ID %d gone. Save the data folder and resume now. \n" %(cur_inst_id))
-
-        # continue
-
-        ### RECOVERY!!!
-        ''' First, save the data folder. '''
-        cur_mysql_data_dir_str = os.path.join(mysql_root_dir, "data_all/data_" + str(cur_inst_id))
-        # cur_mysql_bk_data_dir_str = os.path.join(mysql_root_dir, "data_all/data_bk/")
-        
-        # # check whether the saving folder exists. 
-        # if not os.path.isdir(cur_mysql_bk_data_dir_str):
-        #     os.mkdir(cur_mysql_bk_data_dir_str)
-        
-        # # Find the suitable name for backup folder
-        # for bk_id in range(1000):
-        #     cur_mysql_bk_data_dir_str = os.path.join(mysql_root_dir, "data_all/data_bk/data_" + str(cur_inst_id) + "_" + str(bk_id))
-        #     if not os.path.isdir(cur_mysql_bk_data_dir_str):
-        #         break
-        
-        # try:
-        #     # Save the bk folder. 
-        #     shutil.copytree(cur_mysql_data_dir_str, cur_mysql_bk_data_dir_str)
-        # except shutil.Error as err:
-        #     print("Copy backup data folder failed! %d " % (cur_pid))
-        #     # all_mysql_p_list.pop(cur_pid)
-        #     # break
-
-
-        try:
-            ### DELETE THE ORIGINAL data folder, then reinvoke mysql!
-            if os.path.isdir(cur_mysql_data_dir_str):
-                shutil.rmtree(cur_mysql_data_dir_str)
-            # print("Recovering new data dir: %s to %s"  % (mysql_src_data_dir, cur_mysql_data_dir_str))
-            shutil.copytree(mysql_src_data_dir, cur_mysql_data_dir_str)
-        except shutil.Error as err:
-            print("Copy new data folder failed! Try again later. ID: %d. " % (cur_inst_id))
-            break
-        except OSError as err:
-            print("Copy new data folder failed! Try again later. ID: %d. " % (cur_inst_id))
-            break
-
-
-        # Reinvoke mysql
-        # Prepare for env shared by the fuzzer and mysql. 
-
-        # Set up SQLRight output folder
-        cur_output_dir_str = "./outputs_" + str(cur_inst_id - starting_core_id)
-
-        cur_output_file = os.path.join(cur_output_dir_str, "output.txt")
-        if os.path.isfile(cur_output_file):
-            os.remove(cur_output_file)
-
-        mysql_bin_dir = os.path.join(mysql_root_dir, "bin/mysqld")
-        
-        # Start the MYSQL instance
-        ori_workdir = os.getcwd()
-
-        mysql_command = [
-            "screen",
-            "-dmS",
-            "test" + str(cur_inst_id),
-            "bash", "-c", 
-            "'",    # left quote
-            mysql_bin_dir,
-            "--basedir=" + mysql_root_dir,
-            "--datadir=" + cur_mysql_data_dir_str,
-            "--port=" + str(cur_port_num),
-            "--socket=" + socket_path,
-            "--performance_schema=OFF",
-            "&>", cur_output_file,
-            "'"  # right quote
-        ]
-        mysql_modi_env = dict()
-        mysql_modi_env["__AFL_SHM_ID"] = cur_shm_str
-
-        mysql_command = " ".join(mysql_command)
-
-        print("Running mysql command: __AFL_SHM_ID=" + cur_shm_str + " " + mysql_command, end="\n")
-
-
-        p = subprocess.Popen(
-                            mysql_command,
-                            shell=True,
-                            stderr=subprocess.DEVNULL,
-                            stdout=subprocess.DEVNULL,
-                            stdin=subprocess.DEVNULL,
-                            env = mysql_modi_env
-                            )
-
-        print("Finished running popen. \n")
-        time.sleep(1)
-        
-        cur_output_file_r = os.path.join(cur_output_dir_str, "output.txt")
-
-        if not os.path.isfile(cur_output_file_r):
-            # Failed to boot mysql. Try again later. 
-            print("Waiting for the mysql to reboot. Current output dir is: " + cur_output_file_r + "\n")
-            continue
-
-        # Break the loop. Do not continue in this round. In case of race condition for all_mysql_p_list
-        break
-
-    # SHUTDOWN MYSQL, periodically. 
-    for prev_shutdown_time_idx in range(len(all_prev_shutdown_time)):
-        prev_shutdown_time = all_prev_shutdown_time[prev_shutdown_time_idx]
-
-        if (time.mktime(time.localtime())  -  time.mktime(prev_shutdown_time))  > 60: # 60 sec, restart mysql
-
-            print("******************\nBegin scheduled MYSQL restart. ID: %d\n" % (prev_shutdown_time_idx))
-            # Politely, restart MySQL. 
-            cur_port_num = port_starting_num + prev_shutdown_time_idx - starting_core_id
-            socket_path = "/tmp/mysql_" + str(prev_shutdown_time_idx) + ".sock"
-
-            try:
-                db = MySQLdb.connect(host="localhost",    # your host, usually localhost
-                     user="root",         # your username
-                     passwd="",  # your password
-                     port=cur_port_num,
-                     unix_socket=socket_path,
-                     db="fuck")        # name of the data base
-            except MySQLdb._exceptions.OperationalError:
-                print("MYSQL server down, not recovered yet. \n\n\n")
-                continue
-            
-            cur = db.cursor()
-
-            cur.execute("SHUTDOWN;")
-
-            db.close()
-
-            time.sleep(2)
-
-            # Update shutdown time. 
-            all_prev_shutdown_time[prev_shutdown_time_idx] = time.localtime()
-
-            print("MYSQL shutdown completed. ID: %d\n\n\n" % (prev_shutdown_time_idx))
-
-            # 2 more seconds would be waited until the new MYSQL is being started. Thus, there would be more than 2 seconds between every MYSQL process restart. 
-            # The actual restart of mysql is being handle by the same MYSQL crash handler. (Above)
-            continue
-
-
-            
-
+    time.sleep(100)
