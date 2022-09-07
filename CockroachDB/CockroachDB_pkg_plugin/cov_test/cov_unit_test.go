4607c7de35e6a112a7dbcd8b85bb2373c392a586
Yu Liang <luy70@psu.edu>
2022-09-06 21:36:22 -0400

    CockroachDB pkg updates.

--- CockroachDB/CockroachDB_pkg_plugin/cov_test/cov_unit_test.go
+++ CockroachDB/CockroachDB_pkg_plugin/cov_test/cov_unit_test.go
@@ -12,6 +12,69 @@ import (
 	"testing"
 )
 
+//func Fuzz(data []byte) int {
+//
+//	params, _ := tests.CreateTestServerParams()
+//	s, sqlDB, _ := serverutils.StartServer(t, params)
+//	defer s.Stopper().Stop(context.Background())
+//	//ctx := context.Background()
+//
+//	sqlRun := sqlutils.MakeSQLRunner(sqlDB)
+//
+//	//	testingQuery := `
+//	//        CREATE DATABASE t;
+//	//        CREATE TABLE t.kv (k CHAR PRIMARY KEY, v CHAR, FAMILY (k), FAMILY (v));
+//	//        INSERT INTO t.kv VALUES ('c', 'e'), ('a', 'c'), ('b', 'd');
+//	//		SELECT * FROM t.kv;
+//	//`
+//
+//	log.Printf("Debug: Inside the coverage unit test. \n")
+//
+//	// Read from local file.
+//	out_file, out_err := os.Create("./res_out.txt")
+//	if out_err != nil {
+//		panic(out_err)
+//	}
+//	defer out_file.Close()
+//
+//	// Separate the input queries. If single query fails,
+//	// the query sequence execution can still continue.
+//	testingQueryArray := strings.Split(string(data), "\n")
+//
+//	for idx, testingQuery := range testingQueryArray {
+//
+//		log.Printf("\n\nDebug: Running on statement %d: %s", idx, testingQuery)
+//		out_file.WriteString(fmt.Sprintf("Res IDX: %d\n", idx))
+//
+//		// Higher lever query running: resRows := sqlRun.Query(t, testingQuery)
+//
+//		// QueryContext is the most fundamental function to execute one query
+//		// and returns results. If error happens, this function would only
+//		// return the error rather than throw a Fatal Error.
+//		resRows, err := sqlRun.DB.QueryContext(context.Background(), testingQuery)
+//
+//		if err != nil {
+//			log.Printf("Debug: Getting Error: %s", err)
+//			out_file.WriteString(fmt.Sprintf("%s\n", err))
+//		} else {
+//			log.Printf("Debug: Before RowstoStr. \n")
+//			tmpR, err := sqlutils.RowsToStrMatrix(resRows)
+//
+//			if err != nil {
+//				log.Printf("Debug: Error getting: %s", err)
+//			} else {
+//				r := sqlutils.MatrixToStr(tmpR)
+//				log.Printf("Debug: Getting results %s", r)
+//
+//				out_file.WriteString(fmt.Sprintf("%s", r))
+//			}
+//		}
+//
+//		log.Printf("Debug: Current query execution End. ")
+//	}
+//	return 0
+//}
+
 func TestCov(t *testing.T) {
 
 	params, _ := tests.CreateTestServerParams()
@@ -79,4 +142,3 @@ func TestCov(t *testing.T) {
 	}
 
 }
-
---
+++ CockroachDB/CockroachDB_pkg_plugin/global_cov/global_cov.go
@@ -0,0 +1,35 @@
+package global_cov
+
+import (
+	"bytes"
+	"encoding/binary"
+)
+
+var GCov GlobalCovInfo
+
+type GlobalCovInfo struct {
+	// Default PrevLoc value is 0.
+	prevLoc uint32
+	buf     bytes.Buffer
+}
+
+func (g *GlobalCovInfo) logGlobalCov(curLoc uint32) {
+	if g.buf.Len() == 0 {
+		// The GoLang buffer to uint transfer only support uint16 as smallest.
+		// Each cov value takes two bytes. Thus 2 ^ (n + 1).
+		g.buf.Grow(1 << 19)
+	}
+
+	offset := 2 * g.getXorOffset(curLoc)
+	count := binary.BigEndian.Uint16(g.buf.Bytes()[offset : offset+2])
+	count += 1
+	binary.BigEndian.PutUint16(g.buf.Bytes()[offset:offset+2], count)
+}
+
+func (g *GlobalCovInfo) getXorOffset(curLoc uint32) uint32 {
+	// Get the offset result
+	res := g.prevLoc ^ curLoc
+	// Save the right-shifted curLoc
+	g.prevLoc = curLoc >> 1
+	return res
+}
