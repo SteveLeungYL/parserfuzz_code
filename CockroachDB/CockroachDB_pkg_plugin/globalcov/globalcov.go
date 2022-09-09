71429cce02f53dbd1f7cbb3fee01b29926f226ad
Yu Liang <luy70@psu.edu>
2022-09-09 14:24:05 -0400

    WIP

--- CockroachDB/CockroachDB_pkg_plugin/globalcov/globalcov.go
+++ CockroachDB/CockroachDB_pkg_plugin/globalcov/globalcov.go
@@ -3,7 +3,6 @@ package globalcov
 import (
 	"bytes"
 	"encoding/binary"
-	"log"
 )
 
 var GCov GlobalCovInfo
@@ -22,7 +21,7 @@ func LogGlobalCov(curLoc uint32) {
 	}
 
 	offset := 2 * getXorOffset(curLoc)
-    log.Printf("Logging for offset: input: %d, offset: %d", curLoc, offset)
+    //log.Printf("Logging for offset: input: %d, offset: %d", curLoc, offset)
 	count := binary.BigEndian.Uint16(GCov.buf.Bytes()[offset : offset+2])
 	count += 1
 	binary.BigEndian.PutUint16(GCov.buf.Bytes()[offset:offset+2], count)
