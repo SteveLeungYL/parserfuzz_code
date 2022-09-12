ba45f627a4066fa5acb19d7ab8e1ee34a40d81e5
Yu Liang <luy70@psu.edu>
2022-09-12 14:31:21 -0400

    Even though Golang is only able to transfer UInt16 values. Output the map as UInt8. Each branch trigger uses one byte only.

--- CockroachDB/docker/CockroachDB_pkg_plugin/globalcov/globalcov.go
+++ CockroachDB/docker/CockroachDB_pkg_plugin/globalcov/globalcov.go
@@ -12,8 +12,9 @@ var GCov GlobalCovInfo
 
 type GlobalCovInfo struct {
 	// Default PrevLoc value is 0.
-	prevLoc uint32
-	buf     bytes.Buffer
+    prevLoc uint32        // Max: 262143 
+	buf     bytes.Buffer  // 2 ** 19 buffer size. 
+    sBuf    bytes.Buffer  // 2 ** 18 buffer size. 
 }
 
 func LogGlobalCov(curLoc uint32) {
@@ -35,9 +36,8 @@ func LogGlobalCov(curLoc uint32) {
 
 	count := binary.BigEndian.Uint16(GCov.buf.Bytes()[offset : offset+2])
 	count += 1
-	binary.BigEndian.PutUint16(GCov.buf.Bytes()[offset:offset+2], count)
+	binary.BigEndian.PutUint16(GCov.buf.Bytes()[offset : offset+2], count)
 
-	return
 }
 
 func getXorOffset(curLoc uint32) uint32 {
@@ -51,12 +51,25 @@ func getXorOffset(curLoc uint32) uint32 {
 func SaveGlobalCov() {
     // Plot the coverage output. 
     log.Printf("Inside SaveGlobalCov function. ")
+
+	if GCov.sBuf.Len() == 0 {
+		// The GoLang buffer to uint transfer only support uint16 as smallest.
+		// Each cov value takes two bytes. Thus 2 ^ (n + 1).
+		GCov.sBuf.Grow(1 << 18)
+	}
+
+    curSBuf := GCov.sBuf.Bytes()
+    curBuf := GCov.buf.Bytes()
+    for idx, _ := range(curSBuf) {
+        curSBuf[idx] = curBuf[idx * 2]
+    }
+
 	covFile, covOutErr := os.Create("./cov_out.bin")
 	if covOutErr != nil {
 		panic(covOutErr)
 	}
     defer covFile.Close()
     //log.Printf(fmt.Sprintf("The first few bytes are: %08b,%08b,%08b,%08b", GCov.buf.Bytes()[0], GCov.buf.Bytes()[1], GCov.buf.Bytes()[2], GCov.buf.Bytes()[3]))
-    binary.Write(covFile, binary.LittleEndian, GCov.buf.Bytes())
+    binary.Write(covFile, binary.LittleEndian, GCov.sBuf.Bytes())
     //covFile.Write(GCov.buf.Bytes())
 }
