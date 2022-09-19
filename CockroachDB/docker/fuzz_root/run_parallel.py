199a2b4b1e3137a7f1a8168e8401f7fdfbf5ebfd
Yu Liang <luy70@psu.edu>
2022-09-19 11:29:26 -0400

    Multiple fuzzing in one Docker instance.

--- CockroachDB/docker/fuzz_root/run_parallel.py
+++ CockroachDB/docker/fuzz_root/run_parallel.py
@@ -3,11 +3,12 @@ import os
 import subprocess
 import getopt
 import sys
+import shutil
 
 current_workdir = os.getcwd()
 
 starting_core_id = 0
-parallel_num = 1
+parallel_num = 5
 port_starting_num = 7000
 
 # Parse the command line arguments:
@@ -46,32 +47,44 @@ for cur_inst_id in range(starting_core_id, starting_core_id + parallel_num, 1):
     print("Setting up core_id: " + str(cur_inst_id))
 
     # Set up SQLRight output folder
-    cur_output_dir_str = ""
+    cur_workdir = ""
     if output_dir_str != "":
-        cur_output_dir_str = output_dir_str + "/outputs_"  + str(cur_inst_id - starting_core_id)
+        cur_workdir = output_dir_str + "/outputs_"  + str(cur_inst_id - starting_core_id)
     else:
-        cur_output_dir_str = "./outputs/outputs_" + str(cur_inst_id - starting_core_id)
-    if not os.path.isdir(cur_output_dir_str):
-        os.mkdir(cur_output_dir_str)
+        cur_workdir = "./outputs/outputs_" + str(cur_inst_id - starting_core_id)
+    if output_dir_str != "" and not os.path.isdir(output_dir_str):
+        os.mkdir(output_dir_str)
+    elif not os.path.isdir("./outputs"):
+        os.mkdir("outputs")
+    if not os.path.isdir(cur_workdir):
+        os.mkdir(cur_workdir)
 
-    cur_output_file = os.path.join(cur_output_dir_str, "output.txt")
+    # Copy everything to the working dir. 
+    shutil.copy2("./afl-fuzz", os.path.join(cur_workdir, "afl-fuzz"))
+    shutil.copy2("./covtest.test", os.path.join(cur_workdir, "covtest.test"))
+    shutil.copyfile("./global_data_lib", os.path.join(cur_workdir, "./global_data_lib"))
+    shutil.copyfile("./safe_generate_type", os.path.join(cur_workdir, "./safe_generate_type"))
+    shutil.copytree("./inputs", os.path.join(cur_workdir, "inputs"))
+    shutil.copytree("./cockroach_initlib", os.path.join(cur_workdir, "./cockroach_initlib"))
+
+    cur_output_file = os.path.join(cur_workdir, "output.txt")
     cur_output_file = open(cur_output_file, "w")
     
     # Prepare for env shared by the fuzzer and cockroach. 
     cur_port_num = port_starting_num + cur_inst_id - starting_core_id
 
     # Start running the SQLRight fuzzer. 
-    fuzzing_command = "./afl-fuzz -t 2000 -m 2000 " \
+    fuzzing_command = "./afl-fuzz -t 2000 -m 8000 " \
                         + " -P " + str(cur_port_num) \
                         + " -i ./inputs " \
-                        + " -o " + cur_output_dir_str \
+                        + " -o " + "./" \
                         + " -c " + str(cur_inst_id) \
                         + " -O " + oracle_str
 
     if feedback_str != "":
         fuzzing_command += " -F " + feedback_str
 
-    fuzzing_command +=  " aaa " + " & "
+    fuzzing_command +=  " aaa " + " & "  ### Anything following. Get dump aaa. 
 
     modi_env = dict()
     modi_env["AFL_I_DONT_CARE_ABOUT_MISSING_CRASHES"] = "1"
@@ -81,7 +94,7 @@ for cur_inst_id in range(starting_core_id, starting_core_id + parallel_num, 1):
 
     p = subprocess.Popen(
                         [fuzzing_command],
-                        cwd=os.getcwd(),
+                        cwd=cur_workdir,
                         shell=True,
                         stderr=subprocess.DEVNULL,
                         stdout=subprocess.DEVNULL,
