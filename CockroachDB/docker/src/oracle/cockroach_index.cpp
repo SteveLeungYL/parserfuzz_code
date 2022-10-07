77742c569fd3daa665c7efdd8a85f2d712271ec0
Yu Liang <luy70@psu.edu>
2022-10-07 15:38:29 -0400

    Index oracle. WIP

---
+++ CockroachDB/docker/src/oracle/cockroach_index.cpp
@@ -0,0 +1,5 @@
+//
+// Created by Yu Liang on 10/7/22.
+//
+
+#include "cockroach_index.h"
---
+++ CockroachDB/docker/src/oracle/cockroach_index.h
@@ -0,0 +1,16 @@
+//
+// Created by Yu Liang on 10/7/22.
+//
+
+#ifndef SRC_COCKROACH_INDEX_H
+#define SRC_COCKROACH_INDEX_H
+
+
+
+class cockroach_index {
+
+};
+
+
+
+#endif //SRC_COCKROACH_INDEX_H
