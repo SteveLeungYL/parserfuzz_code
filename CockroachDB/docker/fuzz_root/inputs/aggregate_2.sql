af93457d1d3cfc90ec7eeae8abdbb1793cb8f9c5
Yu Liang <luy70@psu.edu>
2022-10-31 15:01:57 -0400

    Remove one input test that timeout.

--- CockroachDB/docker/fuzz_root/inputs/aggregate_2.sql
+++
@@ -1,5 +0,0 @@
-CREATE TABLE xy(x STRING, y STRING);
-INSERT INTO ab(a,b) VALUES (1,2), (3,4);
-INSERT INTO xy(x, y) VALUES ('a', 'b'), ('c', 'd');
-INSERT INTO index_tab (VALUES ('US_WEST', 3), ('US_EAST', 23), ('US_EAST', -14), ('ASIA', 3294), ('ASIA', -3), ('US_WEST', 31), ('EUROPE', 123), ('US_EAST', -3000));
-SELECT max(data) FROM index_tab WHERE region = 'US_WEST' OR region = 'US_EAST';
