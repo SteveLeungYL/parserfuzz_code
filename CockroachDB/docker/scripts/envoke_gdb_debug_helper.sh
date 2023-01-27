d89a7c30fb3084f97eb830446bd7a0620bbbd095
Yu Liang <luy70@psu.edu>
2023-01-27 13:30:07 -0500

    Added the gdb debugging script.

---
+++ CockroachDB/docker/scripts/envoke_gdb_debug_helper.sh
@@ -0,0 +1,8 @@
+#!/bin/bash
+
+export AFL_I_DONT_CARE_ABOUT_MISSING_CRASHES=1
+export AFL_SKIP_CPUFREQ=1
+
+cd /home/cockroach/fuzzing/fuzz_root/outputs/outputs_$1/
+
+gdb -ex=r --args ./afl-fuzz -t 2000 -m 8000 -P 700$1 -i ./inputs -o ./ -c $1 -O OPT aaa &> output.txt
