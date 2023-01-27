ef2b17d74cb04f55bd5c9a87f079c8d7d71a3eb0
Yu Liang <luy70@psu.edu>
2023-01-27 13:36:58 -0500

    Added auto_run_gdb_test script.

---
+++ CockroachDB/scripts/auto_run_gdb_test.py
@@ -0,0 +1,13 @@
+import os
+import libtmux
+
+server = libtmux.Server()
+
+session = server.new_session(session_name="fuzzing_gdb_debug", kill_session=True, attach=False)
+
+for i in range(8):
+    cur_window = session.new_window(attach=True, window_name="fuzzing_test_"+str(i))
+    cur_pane = cur_window.attached_pane
+    cur_pane.send_keys('sudo docker exec -it sqlright_testing /bin/bash /home/cockroach/scripts/envoke_gdb_debug_helper.sh')
+
+server.attach_session(target_session="fuzzing_gdb_debug")
