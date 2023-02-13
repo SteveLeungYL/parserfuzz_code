eee00af0c339023918d64a4f9b04c98e649cd0e0
Yu Liang <luy70@psu.edu>
2023-02-13 15:26:58 -0500

    (feat) constr_func_sample_testing. Sample testing the function signatures constructed from PostgreSQL outputs.

--- PostgreSQL/docker/src/AFL/sample_func_opr_data_types.cpp
+++ PostgreSQL/docker/src/AFL/sample_func_opr_data_types.cpp
@@ -10,254 +10,7 @@
 #include <vector>
 
 #include "../include/data_type_signatures.h"
-
-// Required for the PostgreSQL connection.
-#include "libpq-fe.h"
-#include "../include/utils.h"
-
-using namespace std;
-
-int bind_to_port = 5432;
-
-enum SQLSTATUS
-{
-  kConnectFailed,
-  kExecuteError,
-  kServerCrash,
-  kNormal,
-  kTimeout,
-  kSyntaxError,
-  kSemanticError
-};
-
-struct POSTGRES_OUTPUT {
-  string outputs = "";
-  SQLSTATUS status;
-};
-
-static void exit_nicely(PGconn *conn) {
-  PQfinish(conn);
-  exit(1);
-}
-
-static void setting_failed(PGconn *conn) {
-  cerr << "Unable to enter single row mode: " << PQerrorMessage(conn) << endl;
-  exit_nicely(conn);
-}
-
-class PostgresClient
-{
-public:
-  PostgresClient() : counter_(0) {}
-
-  PGconn *connect()
-  {
-    string conninfo = "postgresql://localhost?port=" + to_string(bind_to_port) + "&dbname=x";
-    return PQconnectdb(conninfo.c_str());
-  }
-
-  void disconnect(PGconn *conn)
-  {
-    PQfinish(conn);
-    counter_++;
-  }
-
-  POSTGRES_OUTPUT execute(string cmd)
-  {
-    PGresult *res;
-    POSTGRES_OUTPUT result;
-    std::ostringstream outputStream;
-    result.outputs = "";
-    int first = 1;
-    int nFields;
-    int i, j;
-    int execute_ok = 0;
-
-    conn = connect();
-
-    // if (PQstatus(conn) != CONNECTION_OK) {
-    //   // disconnect(conn);
-    //   conn = connect();
-    if (PQstatus(conn) != CONNECTION_OK) {
-      disconnect(conn);
-      result.status = kConnectFailed;
-      return result;
-    }
-    // }
-
-    reset_database(conn);
-
-    vector<string> cmd_vec = string_splitter(cmd, ";");
-    vector<string> timeout_cmd_vec = {"set statement_timeout to 200; "};
-    timeout_cmd_vec.insert(timeout_cmd_vec.end(), cmd_vec.begin(), cmd_vec.end());
-    cmd_vec = timeout_cmd_vec;
-
-    time_t begin_timer;
-    time_t process_timer;
-    bool is_timeout = false;
-    time(&begin_timer);
-    // Loop through the cmd_vec.
-    for (string& cur_cmd : cmd_vec) {
-      cur_cmd += "; ";
-      /* Send our statements off to the server. */
-      if (!PQsendQuery(conn, cur_cmd.c_str())) {
-        cerr << "Sending statements to server failed: " << PQerrorMessage(conn) << endl;
-        // exit_nicely(conn);
-      }
-
-      if (check_status(conn) == false) {
-        cerr << "In func execute(), we get kServerCrash. \n";
-        result.status = kServerCrash;
-        return result;
-      }
-
-      /* We want results row-by-row. */
-      if (!PQsetSingleRowMode(conn)) {
-        setting_failed(conn);
-      }
-
-      /* Loop through the results of our statements. */
-      while ( !is_timeout  &&  (res = PQgetResult(conn)) && res != NULL ) {
-        switch (PQresultStatus(res)) {
-        case PGRES_COMMAND_OK: {
-          /* a query command that doesn't return
-              * anything was executed properly by the
-              * backend */
-          break;
-        }
-        case PGRES_TUPLES_OK:  {/* No more rows from current query. */
-          /* We want the next statement's results row-by-row also. */
-          if (!PQsetSingleRowMode(conn)) {
-            PQclear(res);
-            setting_failed(conn);
-          }
-          first = 1;
-          break;
-        }
-        case PGRES_SINGLE_TUPLE: {
-          if (first) {
-            /* Produce a "nice" header" */
-            // cout << "-----------------------------"
-            //         "-----------------------------"
-            //         << endl
-            //         << "Results of statement number:" << endl;
-            /* print out the attribute names */
-            nFields = PQnfields(res);
-            // for (i = 0; i < nFields; i++) {
-            //   cout << "PQfname: " << PQfname(res, i) << endl;
-            //   outputStream << PQfname(res, i) << " ";
-            // }
-            // outputStream << endl;
-            first = 0;
-          }
-          /* print out the row */
-          for (j = 0; j < nFields; j++) {
-            // cout << "PQgetvalue: " << PQgetvalue(res, 0, j) << endl;
-            // outputStream << PQgetvalue(res, 0, j) << " ";
-            const char* res_char = PQgetvalue(res, 0, j);
-            if (res_char != NULL && !PQgetisnull(res, 0, j)) {
-              result.outputs += string(res_char) + " ";
-            }
-            time(&process_timer);
-            double run_seconds = difftime(process_timer, begin_timer);
-            // cerr << "Getting timeout run_seconds: " << run_seconds << "\n\n\n";
-            if (run_seconds > 2.0) {
-              result.outputs = "Error: Execution is Timeout. ";
-              PGcancel* cancel_conn = PQgetCancel(conn);
-              char errbuf[512];
-              PQcancel(cancel_conn, errbuf, 512);
-              PQfreeCancel(cancel_conn);
-              is_timeout = true;
-              break;
-            }
-          }
-          result.outputs += "\n";
-          // outputStream << endl;
-          // cerr << "result.outputs is: " << result.outputs << "\n\n\n";
-          // result.outputs = outputStream.str();
-          // execute_ok += 1;
-          break;
-        }
-        case PGRES_FATAL_ERROR: {
-          // disconnect(conn);
-          result.status = kExecuteError;
-
-          string error_msg = PQerrorMessage(conn);
-          result.outputs += error_msg + "\n";
-        }
-        default: {
-          /* Always call PQgetResult until it returns null, even on
-            * error. */
-          // cerr << "Query execution failed: " << PQerrorMessage(conn) << endl;
-          // cerr << "PQresultStatus: " << PQresultStatus(res) << endl;
-          // postgre_execute_error += 1;
-        }
-        } // switch
-        PQclear(res);
-      }  // while ( (res = PQgetResult(conn))  && !is_timeout)
-      if (is_timeout) {  // Timeout, ignore the following SQL commands.
-        break;
-      }
-    } // for (string& cur_cmd : cmd_vec)
-
-    disconnect(conn);
-    result.status = kNormal;
-
-    // getchar();
-    return result;
-  }
-
-  void reset_database(PGconn *conn)
-  {
-    PGresult *res = PQexec(conn, "SET client_min_messages TO WARNING;DROP SCHEMA public CASCADE; CREATE SCHEMA public;");
-    PQclear(res);
-  }
-
-  void drop_database(PGconn *conn)
-  {
-    if (counter_ % 2 == 0){
-      PGresult *res = PQexec(conn, "DROP DATABASE IF EXISTS test;");
-      PQclear(res);
-    }
-    else {
-      PGresult *res = PQexec(conn, "DROP DATABASE IF EXISTS test2;");
-      PQclear(res);
-    }
-  }
-
-  void create_database(PGconn *conn)
-  {
-    if (counter_ % 2 == 0) {
-      PGresult *res = PQexec(conn, "CREATE DATABASE test;");
-      PQclear(res);
-    }
-    else {
-      PGresult *res = PQexec(conn, "CREATE DATABASE test2;");
-      PQclear(res);
-    }
-  }
-
-  bool check_status(PGconn *conn)
-  {
-    auto res = PQstatus(conn);
-    if (res == CONNECTION_OK)
-      return true;
-
-    return false;
-  }
-
-  char *get_next_database_name()
-  {
-    if (counter_ % 2 == 0)
-      return "test2";
-
-    return "test";
-  }
-
-private:
-  unsigned counter_; //odd for "test", even for "test2"
-  PGconn *conn;
-};
+#include "../include/postgres_connector.h"
 
 PostgresClient g_psql_client;
 
@@ -369,10 +122,38 @@ void init_all_func_sig(vector<FuncSig>& v_func_sig) {
   return;
 }
 
+void constr_func_sample_testing(vector<FuncSig>& v_func_sig) {
+  // For every saved functions, sample the function from running them in the
+  // PostgreSQL DBMS. Log the validity rate.
+  // Ad-hoc implementation. Please make it mature before moving it to the main
+  // afl-fuzz fuzzer.
+
+  for (FuncSig cur_func: v_func_sig) {
+
+    string cmd_str;
+    // Ad-hoc create table statement.
+    cmd_str +=
+        "CREATE TABLE v0 (c1 int, c2 bigint, c3 bigserial, c4 bit[3], c5 varbit[5], "
+        "c6 bool, c7 bytea, c8 char[3], c9 varchar[5], c10 cidr, c11 date, "
+        "c12 float, c13 inet, c14 interval, c15 json, c16 jsonb, c17 macaddr, "
+        "c18 macaddr8, c19 money, c20 numeric, c21 real, c22 smallint, "
+        "c23 smallserial, c24 serial, c25 text, c26 time, c27 timetz, "
+        "c28 timestamp, c29 timestamptz, c30 uuid, c31 tsquery, c32 tsvector, "
+        //             "c33 txidsnapshot, " // Not existed.
+        "c34 xml, c35 box, c36 circle, c37 line, "
+        "c38 point, c39 polygon, c40 oid);";
+  }
+
+  return;
+
+}
+
 int main() {
 
   vector<FuncSig> v_func_sig;
   init_all_func_sig(v_func_sig);
+
+  constr_func_sample_testing(v_func_sig);
 
   return 0;
 }
