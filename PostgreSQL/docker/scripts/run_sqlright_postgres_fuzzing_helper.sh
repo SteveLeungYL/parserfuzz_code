f2d8523d7166d66c5de3d5a02351aac82643b257
Yu Liang <luy70@psu.edu>
2023-02-15 13:11:41 -0500

    Setup the PostgreSQL script on Docker build.

--- PostgreSQL/docker/scripts/run_sqlright_postgres_fuzzing_helper.sh
+++ PostgreSQL/docker/scripts/run_sqlright_postgres_fuzzing_helper.sh
@@ -6,20 +6,7 @@
 chown -R postgres:postgres /home/postgres/fuzzing
 
 SCRIPT_EXEC=$(cat << EOF
-# Setup data folder
-cd /home/postgres/postgres/bld
-./bin/initdb -D ./data
-./bin/pg_ctl -D ./data start
-./bin/createdb x
-./bin/pg_ctl -D ./data stop
-mkdir -p data_all
-mv data data_all/ori_data
-
-mkdir -p /home/postgres/fuzzing/Bug_Analysis/bug_samples
-
 cd /home/postgres/fuzzing/fuzz_root
-
-cp /home/postgres/src/afl-fuzz ./
 
 printf "\n\n\n\nStart fuzzing. \n\n\n\n\n"
 
