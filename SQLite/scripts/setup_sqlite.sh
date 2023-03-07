9554ccb66c2854fde615363b000ae9e29c720cd4
Yu Liang <luy70@psu.edu>
2023-03-07 14:53:42 -0500

    (fix) forget to push the updated setup_sqlite.sh script.

--- SQLite/scripts/setup_sqlite.sh
+++ SQLite/scripts/setup_sqlite.sh
@@ -1,4 +1,7 @@
 #!/bin/bash -e
 cd "$(dirname "$0")"/../docker
+
+cp -r ../../Common_Tootls/rsg ./rsg
+
 ## Release code. Remove all intermediate steps to save hard drive space.
 sudo docker build --rm=true -f ./Dockerfile -t sqlright_sqlite .
