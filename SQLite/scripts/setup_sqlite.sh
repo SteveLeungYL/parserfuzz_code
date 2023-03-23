ba27d46a257223e109867402e1c9431766c790ef
Yu Liang <luy70@psu.edu>
2023-03-23 15:05:07 -0400

    (fix) Refresh the RSG sqlite_parser_rule_only.y file everytime.

--- SQLite/scripts/setup_sqlite.sh
+++ SQLite/scripts/setup_sqlite.sh
@@ -1,7 +1,11 @@
 #!/bin/bash -e
 cd "$(dirname "$0")"/../docker
 
+rm -rf ./rsg
+rm ./src/sqlite_parser_rule_only.y
+
 cp -r ../../Common_Tootls/rsg ./
+cp ./rsg/sqlite_parse_rule_only.y src/sqlite_parse_rule_only.y
 
 ## Release code. Remove all intermediate steps to save hard drive space.
 sudo docker build --rm=true -f ./Dockerfile -t sqlright_sqlite .
