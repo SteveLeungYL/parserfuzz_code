9d83e6fb5a98786012151e7cd35cd12a981f2610
Yu Liang <luy70@psu.edu>
2023-03-24 15:12:04 -0400

    (feat) Added data type related code to the SQLite fuzzer, working on the Function Signature building.

--- SQLite/docker/src/include/define.h
+++ SQLite/docker/src/include/define.h
@@ -308,6 +308,65 @@
   V(OverClause)                                                                \
   V(FilterClause)
 
+#define ALLDATATYPE(V)                                                         \
+  V(TYPEUNKNOWN)                                                               \
+  V(TYPEUNDEFINE)                                                              \
+  V(TYPEANY)                                                                   \
+  V(TYPENONE)                                                                  \
+  V(TYPEIDENT)                                                                 \
+  V(TYPEVOID)                                                                  \
+  V(TYPEBIGINT)                                                                \
+  V(TYPEBIGSERIAL)                                                             \
+  V(TYPEBIT)                                                                   \
+  V(TYPEVARBIT)                                                                \
+  V(TYPEBOOL)                                                                  \
+  V(TYPEBYTEA)                                                                 \
+  V(TYPECHAR)                                                                  \
+  V(TYPEVARCHAR)                                                               \
+  V(TYPECIDR)                                                                  \
+  V(TYPEDATE)                                                                  \
+  V(TYPEFLOAT)                                                                 \
+  V(TYPEINET)                                                                  \
+  V(TYPEINT)                                                                   \
+  V(TYPEINTERVAL)                                                              \
+  V(TYPEJSON)                                                                  \
+  V(TYPEJSONB)                                                                 \
+  V(TYPEMACADDR)                                                               \
+  V(TYPEMACADDR8)                                                              \
+  V(TYPEMONEY)                                                                 \
+  V(TYPENUMERIC)                                                               \
+  V(TYPEREAL)                                                                  \
+  V(TYPESMALLINT)                                                              \
+  V(TYPESMALLSERIAL)                                                           \
+  V(TYPESERIAL)                                                                \
+  V(TYPETEXT)                                                                  \
+  V(TYPETIME)                                                                  \
+  V(TYPETIMETZ)                                                                \
+  V(TYPETIMESTAMP)                                                             \
+  V(TYPETIMESTAMPTZ)                                                           \
+  V(TYPEUUID)                                                                  \
+  V(TYPEOID)                                                                   \
+  /* Separator line. Do not auto generate the types below this line. */        \
+  V(TYPECSTRING)                                                               \
+  /* Separator line. Do not support the types below this line. */              \
+  V(TYPENOTSUPPORT)                                                            \
+  V(TYPETSQUERY)                                                               \
+  V(TYPETSVECTOR)                                                              \
+  V(TYPETXIDSNAPSHOT)                                                          \
+  V(TYPEXML)                                                                   \
+  V(TYPEENUM)                                                                  \
+  V(TYPETUPLE)                                                                 \
+  V(TYPEBOX)                                                                   \
+  V(TYPECIRCLE)                                                                \
+  V(TYPELINE)                                                                  \
+  V(TYPELSEG)                                                                  \
+  V(TYPEPATH)                                                                  \
+  V(TYPEPGLSN)                                                                 \
+  V(TYPEPGSNAPSHOT)                                                            \
+  V(TYPEPOINT)                                                                 \
+  V(TYPEPOLYGON)
+
+
 #define SAFETRANSLATE(a) (assert(a != NULL), a->translate(v_ir_collector))
 
 #define SAFEDELETE(a)                                                          \
