5a1582d64bd23af26085f5859c4f0987858d313f
Yu Liang <luy70@psu.edu>
2023-06-20 21:56:57 -0400

    (feat) Sort the crash file name based on file creation time.

--- MySQL/docker/scripts/prepare_deduplicate_bugs.py
+++ MySQL/docker/scripts/prepare_deduplicate_bugs.py
@@ -22,8 +22,9 @@ for i in range (100):
     for cur_file in os.listdir(cur_crash_dir):
         if "README.txt" in cur_file:
             continue
-        shutil.copy(os.path.join(cur_crash_dir, cur_file), os.path.join(dest_copy_dir, f"{i}_"+cur_file))
-        print(f'Copy from {os.path.join(cur_crash_dir, cur_file)} to {os.path.join(dest_copy_dir, f"{i}_"+cur_file)}\n\n')
+        cur_file_creation_time = os.path.getctime(os.path.join(cur_crash_dir, cur_file))
+        shutil.copy(os.path.join(cur_crash_dir, cur_file), os.path.join(dest_copy_dir, f"{round(cur_file_creation_time)}_{i}_"+cur_file))
+        print(f'Copy from {os.path.join(cur_crash_dir, cur_file)} to {os.path.join(dest_copy_dir, f"{round(cur_file_creation_time)}_{i}_"+cur_file)}\n\n')
 
 print("""
 Copy finished. Now executing the following script: 
