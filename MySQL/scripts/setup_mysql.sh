61edf53cc5095aef298d7241d267fa64caf0cfb1
“ <luy70@psu.edu>
2023-06-29 15:46:13 -0400

    (fix) Revert some changes in the main branch that we don't want.

--- MySQL/scripts/setup_mysql.sh
+++ MySQL/scripts/setup_mysql.sh
@@ -12,7 +12,5 @@ cp -r ../../Common_Tootls/AFLTriage ./AFLTriage
 #sudo docker build --rm=false -f ./Dockerfile -t sqlright_mysql .  
 
 ## Release code. Remove all intermediate steps to save hard drive space.
-sudo docker build --rm=true -f ./Dockerfile -t sqlright_mysql_main .
-
-
+sudo docker build --rm=true -f ./Dockerfile -t sqlright_mysql .
 
