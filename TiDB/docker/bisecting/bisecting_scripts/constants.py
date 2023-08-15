7447c60074b554facf3cbe9e8937ffa7ceac27e5
“ <luy70@psu.edu>
2023-08-15 14:35:41 -0400

    (feat) Bisecting folder auto setup.

--- TiDB/docker/bisecting/bisecting_scripts/constants.py
+++ TiDB/docker/bisecting/bisecting_scripts/constants.py
@@ -1,19 +1,21 @@
 from enum import Enum
 import os
 
-BISECTING_SCRIPTS_ROOT = "/home/tidb/bisecting_scripts"
+# IMPORTANT VARIBLES to setup
+BISECTING_SCRIPTS_ROOT = "/home/tidb/bisecting"
 TIDB_CACHE_ROOT = "/home/tidb/tidb_binary"
 TIDB_SRC = "/home/tidb/go_projects/src/github.com/tidb/tidb"
 
+TIDB_SERVER_SOCKET = "/tmp/mysql_0.sock"
+TIDB_SERVER_PORT = "8000"
+
+# Auto setup variables
 BUG_SAMPLES_PATH = os.path.join(BISECTING_SCRIPTS_ROOT, "bug_samples")
 LOG_OUTPUT_FILE = os.path.join(BISECTING_SCRIPTS_ROOT, "logs.txt")
 FAILED_COMPILE_COMMITS = os.path.join(BISECTING_SCRIPTS_ROOT, "FAILED_COMPILE_COMMITS.json")
 
 UNIQUE_BUG_JSON = os.path.join(BISECTING_SCRIPTS_ROOT, "unique_bug.json")
 TIDB_SORTED_COMMITS = os.path.join( BISECTING_SCRIPTS_ROOT, "assets/sorted_commits.json")
-
-TIDB_SERVER_SOCKET = "/tmp/mysql_0.sock"
-TIDB_SERVER_PORT = "8000"
 
 class RESULT(Enum):
     PASS = 1
