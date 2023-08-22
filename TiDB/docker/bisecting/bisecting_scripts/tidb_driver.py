9f1b064730983b71903e2c3dc861426a8bd7abb7
“ <luy70@psu.edu>
2023-08-22 14:35:54 -0400

    (fix) Fix a small potential crashing problem for the bisecting scripts.

--- TiDB/docker/bisecting/bisecting_scripts/tidb_driver.py
+++ TiDB/docker/bisecting/bisecting_scripts/tidb_driver.py
@@ -19,7 +19,7 @@ def check_tidb_server_alive() -> bool:
                         stdout=subprocess.PIPE,
                         stderr=subprocess.STDOUT
                         )
-    
+
     res = p.stdout.decode()
     if "tidb-serv" in res:
         return True
@@ -33,7 +33,7 @@ def stop_tidb_server():
                         stderr=subprocess.DEVNULL,
                         stdin=subprocess.DEVNULL
                         )
-    
+
     while (check_tidb_server_alive()):
         time.sleep(1)
         p = subprocess.run("pkill -9 tidb-server",
@@ -55,7 +55,7 @@ def start_tidb_server(hexsha: str):
 
     if not os.path.isdir(cur_tidb_cache_root):
         tidb_builder.setup_tidb_commit(hexsha)
-    
+
     if not os.path.isdir(cur_tidb_cache_root):
         # Failed to compile the current version of MySQL. Return.
         utils.dump_failed_commit(hexsha)
@@ -64,11 +64,11 @@ def start_tidb_server(hexsha: str):
     cur_tidb_cache_root = os.path.join(cur_tidb_cache_root, get_tidb_binary_sub_dir(cur_tidb_cache_root))
     if os.path.isdir(os.path.join(cur_tidb_cache_root, "db_data")):
         # remove the old database folder.
-        shutil.rmtree(os.path.join(cur_tidb_cache_root, "db_data"))
+        shutil.rmtree(os.path.join(cur_tidb_cache_root, "db_data"), ignore_errors=True)
 
     logger.debug("Starting tidb server with hash: %s" % (hexsha))
 
-    # And then, call TiDB server process. 
+    # And then, call TiDB server process.
     tidb_server_launch_command = [
         "./tidb-server",
         f"-P {constants.TIDB_SERVER_PORT}",
@@ -98,11 +98,11 @@ def start_tidb_server(hexsha: str):
             logger.warning("TiDB-Server not alive after 6 seconds. ")
             stop_tidb_server()
             return
-    
+
     return
 
 def execute_queries(query: str, hexsha: str):
-    """Entry function. Call this function to run the mysql server and the client. 
+    """Entry function. Call this function to run the mysql server and the client.
         Run the passed in query and check whether the query crashes the server.
     """
 
@@ -110,7 +110,7 @@ def execute_queries(query: str, hexsha: str):
 
     if not check_tidb_server_alive():
         return constants.RESULT.FAIL_TO_COMPILE
-    
+
     cur_mysql_root = os.path.join(constants.TIDB_CACHE_ROOT, hexsha)
 
     mysql_client = f"mysql -h 127.0.0.1 -P {constants.TIDB_SERVER_PORT} -u root -f --socket={constants.TIDB_SERVER_SOCKET} "
@@ -119,7 +119,7 @@ def execute_queries(query: str, hexsha: str):
     clean_database_query = "DROP DATABASE IF EXISTS test_rsg1; CREATE DATABASE IF NOT EXISTS test_rsg1; "
 
     utils.execute_command(
-        mysql_client, input_contents=clean_database_query, timeout=3  # 3 seconds timeout. 
+        mysql_client, input_contents=clean_database_query, timeout=3  # 3 seconds timeout.
     )
 
     safe_query = "USE test_rsg1; " + query
@@ -129,7 +129,7 @@ def execute_queries(query: str, hexsha: str):
     all_error_msg = ""
 
     output, error_msg, status = utils.execute_query_helper(
-        mysql_client, input_contents=safe_query, timeout=5  # 5 seconds timeout. 
+        mysql_client, input_contents=safe_query, timeout=5  # 5 seconds timeout.
     )
 
     # safe_query itself is far too long. Do not print it.
